9226c2dec5638160c76e74cc14e0472961dc2858
eugeniusms <eugeniusms@gmail.com>
2022-10-11 23:29:47 +0700

    🔧 Add Template and Setup Tailwind via CDN

--- wisata_nusantara/settings.py
+++ wisata_nusantara/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = "wisata_nusantara.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates"
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
