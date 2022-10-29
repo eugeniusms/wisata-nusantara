699ba0f59a692b03ad83af749ea01df348fe48fb
eugeniusms <eugeniusms@gmail.com>
2022-10-30 00:12:46 +0700

    🛠 Change Model Attributes and Dumping User Data

--- daftar_destinasi/admin.py
+++ daftar_destinasi/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from daftar_destinasi.models import Destinasi
 
 # Register your models here.
+admin.site.register(Destinasi)
--- daftar_destinasi/models.py
+++ daftar_destinasi/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Destinasi(models.Model):
   nama = models.CharField(max_length=255)
   deskripsi = models.TextField()
@@ -12,4 +12,4 @@ class Destinasi(models.Model):
   maps_url = models.URLField()
   suka = models.IntegerField(default=0)
   # user related
-  created_by = models.TextField()
+  created_by = models.ForeignKey(User, on_delete=models.CASCADE)
