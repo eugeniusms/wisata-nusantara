63da15ad0f84abad7adcfe99cfed54a719b87d14
Your Name <rizka.nisrina@ui.ac.id>
2022-12-02 13:31:39 +0700

    add json getter for cerita_perjalanan

--- cerita_perjalanan/urls.py
+++ cerita_perjalanan/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from cerita_perjalanan.views import submit_cerita, get_cerita, delete_cerita
+from cerita_perjalanan.views import submit_cerita, get_cerita, delete_cerita, get_cerita_json
 
 app_name = 'cerita_perjalanan'
 
 urlpatterns = [
     path('', get_cerita, name='get-cerita'),
     path('submit/', submit_cerita, name='submit-cerita'),
-    path('delete/<int:id>', delete_cerita, name='delete-cerita')
+    path('delete/<int:id>', delete_cerita, name='delete-cerita'),
+    path('json/', get_cerita_json, name='get-cerita-json')
 ]
