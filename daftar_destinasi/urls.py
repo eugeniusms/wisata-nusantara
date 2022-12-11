33ddde45c2429ced1a9ae0e75ef7ac78a2435154
eugeniusms <eugeniusms@gmail.com>
2022-12-11 10:52:10 +0700

    🔧 Add Route POST Destination From Flutter

--- daftar_destinasi/urls.py
+++ daftar_destinasi/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from daftar_destinasi.views import daftar_destinasi, show_json, show_suka_json, hapus_destinasi_by_id, show_suka_json, sukai_destinasi, show_wishlist
+from daftar_destinasi.views import daftar_destinasi, show_json, show_suka_json, hapus_destinasi_by_id, show_suka_json, sukai_destinasi, show_wishlist, add_destination_flutter
 
 app_name = 'daftar_destinasi'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('suka/<int:id>', sukai_destinasi, name='sukai_destinasi'), # UPDATE
     path('delete/<int:id>', hapus_destinasi_by_id, name='hapus_destinasi_by_id'),
     path('wishlist/', show_wishlist, name='show_wishlist'), # GET
+    path('add-flutter/', add_destination_flutter, name='add_destination_flutter')
 ]
