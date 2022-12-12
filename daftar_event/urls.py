b07ef750ec0065f812184cef57c1e382674063c2
aulianajwa <auliesl99@gmail.com>
2022-12-12 15:04:43 +0700

    add delete flutter

--- daftar_event/urls.py
+++ daftar_event/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from daftar_event.views import add_json, category_event, daftar_event, show_json,delete_event,lihat_event, tambah_event, add_from_flutter
+from daftar_event.views import add_json, category_event, daftar_event, show_json,delete_event,lihat_event, tambah_event, add_from_flutter, delete_from_flutter
 
 app_name = 'daftar_event'
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('delete/<id>', delete_event, name='delete-event'),
     path('lihat-event/<id>',lihat_event, name='lihat-event'),
     path('add_from_flutter/', add_from_flutter, name='add_from_flutter'),
+    path('delete_from_flutter/<id>', delete_from_flutter, name='delete_from_flutter')
 ]
